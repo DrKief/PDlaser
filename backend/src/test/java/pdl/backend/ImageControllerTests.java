20f1d7189718077b4d273ae25cc8147bb0e7bddb
Paraschos Koumasonas <paraschos.koumasonas@u-bordeaux.fr>
2026-03-12 23:14:19 +0100

    ci: finalize pipeline configuration

--- backend/src/test/java/pdl/backend/ImageControllerTests.java
+++ backend/src/test/java/pdl/backend/ImageControllerTests.java
@@ -15,15 +15,13 @@ import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(ImageController.class)
 public class ImageControllerTests {
 
     @MockitoBean
